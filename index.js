d996eb1ab34cd2435fe504d88df370cd0e69e841
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-16 18:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function divide(a, b){
   return(a / b)
 }
 
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function makeInt(n){
+  return(n)
+}
+
